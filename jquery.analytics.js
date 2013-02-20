ccebd75b57add59ffd94080c32759b385663468e
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2013-02-19 23:17:30 -0600

    maintain jquery chainability

--- jquery.analytics.js
+++ jquery.analytics.js
@@ -1,13 +1,13 @@
 /* jquery analytics plugin */
 
-(function( $ ) {
+(function($) {
   $.fn.analytics = function() {
-    this.ready(function() {
+    return this.ready(function() {
       $('*[data-track]').click(function() {
         var data = $(this).data('track');
         _gaq.push(['_trackEvent', data.category, data.action, data.label, data.value]);
       });
     });
   };
-})( jQuery );
+})(jQuery);
 
